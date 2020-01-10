f38b9ffca02eaa3c5508eac322692c1f058dc66e
rama <ramalnkb@gmail.com>
2020-01-10 16:36:08 +0100

    Fixed broken redirect

--- misk/src/app/app-routing.module.ts
+++ misk/src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
   { path: 'security', component: SecurityComponent },
   { path: 'privacy', component: ComingSoonComponent },
   { path: 'terms', component: ComingSoonComponent },
+  { path: 'coming-soon', component: ComingSoonComponent },
   {
     path: '**',
     pathMatch: 'full',
