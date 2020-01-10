879b3a4e4d9b57034a3edf5cc514831be5dbe5a2
rama <ramalnkb@gmail.com>
2020-01-10 16:30:33 +0100

    Tweaked css for mission page

--- misk/src/app/components/mission/mission.component.ts
+++ misk/src/app/components/mission/mission.component.ts
@@ -6,10 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mission.component.css']
 })
 export class MissionComponent implements OnInit {
+  currentYear = new Date().getFullYear();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
