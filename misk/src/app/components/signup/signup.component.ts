bc2f2e4b4d58caefd91127cf6702709dbd3eec74
rama <ramalnkb@gmail.com>
2020-01-12 17:20:01 +0100

    fixed bug for submitting email

--- misk/src/app/components/signup/signup.component.ts
+++ misk/src/app/components/signup/signup.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { FirebaseService } from '../../services/firebase.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
+  animations: [
+    trigger('slideUp', [
+      transition(':enter', [
+        style({ transform: 'translateY(100%)', opacity: 0 }),
+        animate('1s', style({ transform: 'translateY(0)', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateX(0)', opacity: 1 }),
+        animate('1s', style({ transform: 'translateY(100%)', opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class SignupComponent implements OnInit {
+  labelClicked: boolean;
+  thanksLabel: boolean;
+  email: string;
+  errorLabel: boolean;
+  constructor(private firebaseService: FirebaseService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  submit() {
+    this.firebaseService.submitEmail(this.email).then(res => {
+      if (res.id) {
+        this.thanksLabel = true;
+        this.errorLabel = false;
+      } else {
+        this.thanksLabel = false;
+        this.errorLabel = true;
+      }
+    });
   }
-
 }
