5554bbc1d064dddb5e37f428dcbc4149a01020c7
Ran Bleich <ran.bleich@mcmireconsulting.com>
2020-02-12 17:34:56 +0100

    Fixed style issues, updated mission

--- misk/src/app/app.module.ts
+++ misk/src/app/app.module.ts
@@ -1,28 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LandingComponent } from './components/landing/landing.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { environment } from '../environments/environment';
-import { NavComponent } from './components/nav/nav.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { FooterComponent } from './components/footer/footer.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { ContactComponent } from './components/contact/contact.component';
-import { ComingSoonComponent } from './components/coming-soon/coming-soon.component';
-import { WrongRouteComponent } from './components/wrong-route/wrong-route.component';
-import { SecurityComponent } from './components/security/security.component';
-import { MissionComponent } from './components/mission/mission.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { BackgroundRingsComponent } from './components/background-rings/background-rings.component';
+import { ComingSoonComponent } from './components/coming-soon/coming-soon.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { MissionComponent } from './components/mission/mission.component';
+import { NavComponent } from './components/nav/nav.component';
+import { SecurityComponent } from './components/security/security.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { WrongRouteComponent } from './components/wrong-route/wrong-route.component';
 
 @NgModule({
   declarations: [
@@ -47,7 +50,9 @@ import { SignupComponent } from './components/signup/signup.component';
     ReactiveFormsModule,
     MatButtonModule,
     MatToolbarModule,
+    MatSidenavModule,
     MatIconModule,
+    MatListModule,
     HttpClientModule,
     BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
